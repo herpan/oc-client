40f2cfba6a29593d70ac5539be3a2fddfa870563
Herpan <herpan.safari@gmail.com>
2021-09-11 15:09:00 +0700

    update pwa

---
+++ precache-manifest.97e4f8f232b27392d4850ac65f5c360c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a343ef25be71079b2da3",
+    "url": "/css/chunk-vendors.ee14b75c.css"
+  },
+  {
+    "revision": "caa23871d8791a787e254773fe9c7d26",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "f145554446bb594b958129b096304e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4b04f1766a8715d00e",
+    "url": "/js/app.14ad5b01.js"
+  },
+  {
+    "revision": "25165c4a11901d0d3cff",
+    "url": "/js/chunk-2d0c192c.749cc6b6.js"
+  },
+  {
+    "revision": "93aef75a9bf4916cd3c5",
+    "url": "/js/chunk-2d0d6d35.4eca9ce4.js"
+  },
+  {
+    "revision": "d793d8c6bd9eb78389ff",
+    "url": "/js/chunk-2d2086b7.e6f2e6af.js"
+  },
+  {
+    "revision": "a343ef25be71079b2da3",
+    "url": "/js/chunk-vendors.b0ea8413.js"
+  },
+  {
+    "revision": "da501c3c3ffbda2635ebf748de3926a0",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "173c4bbeeafa3edfa71e142455464d93",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
